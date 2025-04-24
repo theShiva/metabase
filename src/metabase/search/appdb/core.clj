cfb6be5faf79ce38f2a6678d8117cb19baf2f23e
Nathan Voxland <nathan@metabase.com>
2025-04-24 09:21:23 -0500

    Run search init logic with cluster lock rather than with quartz

--- src/metabase/search/appdb/core.clj
+++ src/metabase/search/appdb/core.clj
@@ -18,6 +18,7 @@
    [metabase.settings.core :as setting]
    [metabase.settings.deprecated-grab-bag :as public-settings]
    [metabase.util :as u]
+   [metabase.util.cluster-lock :as cluster-lock]
    [metabase.util.json :as json]
    [metabase.util.log :as log]
    [toucan2.core :as t2])
