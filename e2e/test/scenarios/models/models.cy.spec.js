2bac20cf3555dd5815c289e82295800b8774c309
Nathan Voxland <nathan@metabase.com>
2025-04-28 16:27:02 -0500

    Undid incorect push

--- e2e/test/scenarios/models/models.cy.spec.js
+++ e2e/test/scenarios/models/models.cy.spec.js
@@ -354,7 +354,7 @@ describe("scenarios > models", () => {
           .and("contain.text", "Orders");
 
         cy.findByText("Everywhere").click();
-        getResults().should("have.length", 5);
+        getResults().should("have.length", 6);
         cy.findByText("6 results").should("be.visible");
         getResults()
           .eq(0)
